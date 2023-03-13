12a85ccf7d576525a51c10fe55fb3e3299d19c5e
Abayomi Olowu <aayobam@gmail.com>
2023-03-13 09:08:01 +0100

    Update tests.py

--- blog/blog/tests.py
+++ blog/blog/tests.py
@@ -11,26 +11,21 @@ class BlogTest(SimpleTestCase):
         response = self.client.get('/<int:pk>/')
         self.assertEqual(response.status_code, 200)
         
-        
     def test_new_post_status_code(self):
         response = self.client.get('/post/new/')
         self.assertEqual(response.status_code, 200)
-        
         
     def test_edit_post_status_code(self):
         response = self.client.get('/post/<int:pk>/edit/')
         self.assertEqual(response.status_code, 200)
         
-        
     def test_delete_post_status_code(self):
         response = self.client.get('/post/<int:pk>/delete/')
         self.assertEqual(response.status_code, 200)
         
-        
     def test_edit_comment_status_code(self):
         response = self.client.get('/comment/<int:pk>/edit/')
         self.assertEqual(response.status_code, 200)
-        
     
     def test_delete_comment_status_code(self):
         response = self.client.get('/comment/<int:pk>/delete/')
